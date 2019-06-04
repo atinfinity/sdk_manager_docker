e50e7bac214ad5f69fba262a8c0238695cc3cb3a
atinfinity <dandelion1124@gmail.com>
2019-06-05 00:34:33 +0900

    +script

---
+++ launch_container.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --privileged --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume=/dev:/dev:rw \
+           --shm-size=1gb \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+           -u "jetpack"  \
+           jetpack:latest \
+           bash
