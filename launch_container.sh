c2fc5eac439783e9aa150d18e10d5a3aff0f9bee
Anton Gerasimov <tossel@gmail.com>
2020-05-23 11:23:34 +0200

    Persist SDK data between runs

--- launch_container.sh
+++ launch_container.sh
@@ -5,10 +5,16 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+mkdir -p jetpack_home/nvidia
+mkdir -p jetpack_home/Downloads
+JETPACK_HOME=$(realpath ./jetpack_home)
+
 docker run --privileged --rm -it \
            --volume=$XSOCK:$XSOCK:rw \
            --volume=$XAUTH:$XAUTH:rw \
            --volume=/dev:/dev:rw \
+           --volume=$JETPACK_HOME/nvidia:/home/jetpack/nvidia:rw \
+           --volume=$JETPACK_HOME/Downloads:/home/jetpack/Downloads:rw \
            --shm-size=1gb \
            --env="XAUTHORITY=${XAUTH}" \
            --env="DISPLAY=${DISPLAY}" \
