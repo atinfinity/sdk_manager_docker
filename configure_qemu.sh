62428020d8b3a0b6cf065fef2fc59d68d212660d
atinfinity <dandelion1124@gmail.com>
2021-05-01 22:09:55 +0900

    Update configure_qemu.sh

--- configure_qemu.sh
+++ configure_qemu.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Reference: https://github.com/MiroPsota/sdkmanagerGUI_docker
+
 sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sudo update-binfmts --enable qemu-aarch64
 sudo update-binfmts --enable qemu-arm
