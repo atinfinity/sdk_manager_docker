dd9ae9858c942b14fc596e10476f3a0d619b46e3
atinfinity <dandelion1124@gmail.com>
2022-05-29 16:34:23 +0900

    updated script

--- configure_qemu.sh
+++ configure_qemu.sh
@@ -6,3 +6,5 @@ sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sudo update-binfmts --enable qemu-aarch64
 sudo update-binfmts --enable qemu-arm
 sudo update-binfmts --enable qemu-armeb
+
+/bin/bash
